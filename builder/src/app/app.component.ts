71cb595d56ac679896f2cb76b9ae01be7852bd58
Kevin M Ashton <kevin.m.ashton@gmail.com>
2018-10-17 21:55:59 -0600

    Add in base

---
+++ builder/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, ViewEncapsulation, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-momentum-element',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Native
+})
+export class AppComponent {
+  @Input()
+  name = 'friend';
+  focus: string;
+  focusSet = false;
+
+  setFocus(value) {
+    this.focus = value;
+    this.focusSet = true;
+  }
+}
